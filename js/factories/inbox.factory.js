d23d7b8d707e8f35e805df048a3fe283c1fe6211
Syed Aman <sna274@nyu.edu>
2016-11-29 10:10:11 -0500

    add helper commenting

--- js/factories/inbox.factory.js
+++ js/factories/inbox.factory.js
@@ -1,10 +1,15 @@
 // Use Angular factory for server-side communication via HTTP
+
+// inject $http service in InboxFactory
 app.factory('InboxFactory', function InboxFactory($http) {
     // Export object for best practice and explicit internal naming to determine private/public
     var exports = {};
     
+    // create functions to get email messages
     exports.getMessages = function () {
+        // use $http service to GET request emails from JSON database
         return $http.get('json/emails.json')
+            // error handling
             .error(function (data) {
                 console.log('There was a HTTP GET error!', data);
             });

cdfeb4045e6feb0e00c73fc158c085c4c65c915d
Syed Aman <sna274@nyu.edu>
2016-11-29 10:21:06 -0500

    use strict

--- js/controllers/inbox.controller.js
+++ js/controllers/inbox.controller.js
@@ -1,9 +1,11 @@
 // Controller: InboxController
-app.controller('InboxController', function($scope) {
-    // Model and View bindings
-    // Small helper functions not needed elsewhere
 
+// Controllers should only define Model and View bindings
+// and as well as small helper functions not needed elsewhere
+app.controller('InboxController', function($scope) {
+    'use strict';
     // Initialize title property to an array for view to use
     $scope.title = 'Write a title here...';
+    // Log to console upon load
     console.log('Inbox Loaded!');
 });
